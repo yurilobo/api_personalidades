837cc7597b9812a4e12145fa3f46c92195f92a51
Yuri <yuri.anderson.lobo@gamail.com>
2024-01-07 19:02:17 -0300

    Modularizando o codigo com o padrao mvc

--- main.go
+++ main.go
@@ -1,21 +1,11 @@
 package main
 
 import (
+	"api_personalidades/routes"
 	"fmt"
-	"log"
-	"net/http"
 )
-
-func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Home Page")
-}
-
-func HandleRequest() {
-	http.HandleFunc("/", Home)
-	log.Fatal(http.ListenAndServe(":8000", nil))
-}
 
 func main() {
 	fmt.Println("Iniciando o servidor rest com Go")
-	HandleRequest()
+	routes.HandleRequest()
 }

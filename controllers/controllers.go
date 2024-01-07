f09dd61954207d576048918bb94f72a36ae52144
Yuri <yuri.anderson.lobo@gamail.com>
2024-01-07 19:29:17 -0300

    Resposta em json

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,10 +1,15 @@
 package controllers
 
 import (
+	"api_personalidades/models"
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Home Page")
+}
+func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(models.Personalidades)
 }
--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"api_personalidades/models"
 	"api_personalidades/routes"
 	"fmt"
 )
 
 func main() {
+	models.Personalidades = []models.Personalidade{
+		{Nome: "Nome 1", Historia: "Historia 1"},
+		{Nome: "Nome 1", Historia: "Historia 1"},
+	}
+
 	fmt.Println("Iniciando o servidor rest com Go")
 	routes.HandleRequest()
 }

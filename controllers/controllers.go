2d59a7bc474c98189356717935e4e992db3c9219
Yuri <yuri.anderson.lobo@gamail.com>
2024-01-17 23:30:47 -0300

    retornando servico por id

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -5,6 +5,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
@@ -12,4 +15,14 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(models.Personalidades)
+}
+func RetornaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	for _, personalidade := range models.Personalidades {
+		if strconv.Itoa(personalidade.Id) == id {
+			json.NewEncoder(w).Encode(personalidade)
+		}
+	}
 }
--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 
 func main() {
 	models.Personalidades = []models.Personalidade{
-		{Nome: "Nome 1", Historia: "Historia 1"},
-		{Nome: "Nome 1", Historia: "Historia 1"},
+		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
+		{Id: 2, Nome: "Nome 1", Historia: "Historia 1"},
 	}
 
 	fmt.Println("Iniciando o servidor rest com Go")

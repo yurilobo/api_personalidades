849d3701f13bc02443747ef46219c29b19966365
Yuri <yuri.anderson.lobo@gamail.com>
2024-02-11 18:05:47 -0300

    ajustes da api

--- main.go
+++ main.go
@@ -9,7 +9,8 @@ import (
 func main() {
 	models.Personalidades = []models.Personalidade{
 		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
-		{Id: 2, Nome: "Nome 1", Historia: "Historia 1"},
+		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
+		{Id: 3, Nome: "Nome 3", Historia: "Historia 3"},
 	}
 
 	fmt.Println("Iniciando o servidor rest com Go")

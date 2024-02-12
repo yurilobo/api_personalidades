4a861d7b89aa469d423b0fdbd7c4c447c92b7de1
Yuri <yuri.anderson.lobo@gamail.com>
2024-02-12 15:13:46 -0300

    uso do content type

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -14,6 +14,7 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Home Page")
 }
 func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
 	var p []models.Personalidade
 	database.DB.Find(&p)
 	json.NewEncoder(w).Encode(p)

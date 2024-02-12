f8b10bcbd4e938ff135c7aad5fa2131cc2aae15a
Yuri <yuri.anderson.lobo@gamail.com>
2024-02-12 11:10:57 -0300

    docker

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"api_personalidades/database"
 	"api_personalidades/models"
 	"api_personalidades/routes"
 	"fmt"
@@ -10,9 +11,12 @@ func main() {
 	models.Personalidades = []models.Personalidade{
 		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
 		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
-		{Id: 3, Nome: "Nome 3", Historia: "Historia 3"},
 	}
-
+	database.ConectaComBancoDeDados()
 	fmt.Println("Iniciando o servidor rest com Go")
 	routes.HandleRequest()
 }
+
+//aqui nesse commit eu tive alguns problemas,eu tinha um postgress rodando local, apontando para porta 5432, tive que da um stop
+//iniciei o composer na porta 5435 para evitar o conflito, e deu erro
+//depois matei o postgres local, mudei as portadas do docker e rodou
